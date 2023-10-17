01d2100f9bdfa826c05d6c2dc057c910c64ecf34
kiendt <dotrungkien6987@gmail.com>
2023-10-18 06:51:19 +0700

    1810

--- src/features/BaoCaoSuCo/DSSuCoDataGrid.js
+++ src/features/BaoCaoSuCo/DSSuCoDataGrid.js
@@ -14,8 +14,9 @@ import {
   DialogContentText,
   DialogActions,
   Stack,
+  Typography,
 } from "@mui/material";
-import { DataGrid, GridRowsProp, GridColDef } from '@mui/x-data-grid';
+import { DataGrid, GridRowsProp, GridColDef } from "@mui/x-data-grid";
 import { useDispatch, useSelector } from "react-redux";
 import { fDate } from "../../utils/formatTime";
 import { useNavigate } from "react-router-dom";
@@ -74,27 +75,121 @@ function DSSuCoDataGrid() {
   const navigate = useNavigate();
 
   const rows: GridRowsProp = [
-    { id: 1, col1: 'Hello', col2: 'World' },
-    { id: 2, col1: 'DataGridPro', col2: 'is Awesome' },
-    { id: 3, col1: 'MUI', col2: 'is Amazing' },
+    { id: 1, col1: "Hello", col2: "World" },
+    { id: 2, col1: "DataGridPro", col2: "is Awesome" },
+    { id: 3, col1: "MUI", col2: "is Amazing" },
   ];
+  const findTenKhoaById = (khoas, targetId) => {
+    const khoa = khoas.find((item) => item._id === targetId);
+    return khoa ? khoa.TenKhoa : null;
+  };
 
   const columns: GridColDef[] = [
+    { field: "MaBC", headerName: "Mã", resizable: false },
+    { field: "HinhThuc", headerName: "Hình thức", width: 100 },
+    {
+      field: "NgaySuCo",
+      headerName: "Ngày sự cố",
+      width: 150,
+      type: "date",
+      // valueGetter: ({ value }) => value && new Date(value),
+      valueFormatter: (params) => {
+        const date =
+          params.value instanceof Date ? params.value : new Date(params.value);
+        if (!isNaN(date.getTime())) {
+          const day = date.getDate();
+          const month = date.getMonth() + 1;
+          const year = date.getFullYear();
+          return `${day}-${month}-${year}`;
+        } else {
+          return "";
+        }
+      },
+    },
 
-    { field: 'MaBC', headerName: 'Mã', resizable:false },
-    { field: 'HinhThuc', headerName: 'Hình thức', width: 100 },
-    { field: 'NgaySuCo', headerName: 'Ngày sự cố', width: 150, type:'date', valueGetter:({value})=>value&& new Date(value) },
-    { field: 'TenBN', headerName: 'Tên NB', width: 150 },
-    { field: 'KhoaSuCo', headerName: 'Khoa xảy ra sự cố',  type: 'singleSelect',
-     width: 150, valueOptions:khoas ,getOptionValue:(value:any)=>value._id,getOptionLabel:(value:any)=>value.TenKhoa },
-    { field: 'MoTa', headerName: 'Mô tả', width: 150 },
-    { field: 'GiaiPhap', headerName: 'Giải pháp', width: 150 ,},
-    
+    { field: "TenBN", headerName: "Tên NB", width: 150 },
+    {
+      field: "KhoaSuCo",
+      headerName: "Khoa xảy ra sự cố",
+
+      width: 150,
+      // valueGetter: ({ value }) => findTenKhoaById(khoas,value),
+      valueGetter: ({ value }) => value && value.TenKhoa,
+      renderHeader: (params: GridColumnHeaderParams) => (
+        <Typography
+          variant="h4"
+          sx={{ my: 1, fontSize: "1rem", color: "#1939B7" }}
+          textAlign="center"
+        >
+          Khoa
+        </Typography>
+      ),
+    },
+    {
+      field: "MoTa",
+      headerName: "Mô tả của thông báo",
+      width: 150,
+      headerAlign: "center",
+      flex: 1,
+    },
+    {
+      field: "GiaiPhap",
+      headerName: "Giải pháp",
+      width: 150,
+      headerAlign: "center",
+    },
+    {
+      field: "date",
+      headerName: "Year",
+      renderCell: (params: GridRenderCellParams<any, Date>) => (
+        <Stack direction={'row'}>
+          {/* {params.value.getFullYear()} */}
+          <Button
+            variant="contained"
+            size="small"
+            // style={{ marginLeft: 16 }}
+            tabIndex={params.hasFocus ? 0 : -1}
+            onClick={() => navigate(`../suco/${params.id}`)}
+          >
+            Sửa
+          </Button>
+          <Button
+            sx={{ fontSize: "0.6rem" }}
+            size="small"
+            variant="contained"
+            color="error"
+            onClick={() => handleDeleteSuCo(params.id)}
+          >
+            Xóa
+          </Button>
+          <Button
+            sx={{ fontSize: "0.6rem" }}
+            size="small"
+            variant="contained"
+            //   color="error"
+            onClick={() =>
+              handleChangeTrangThai(
+                baocaosucos.find((bc) => bc._id === params.id)
+              )
+            }
+          >
+            {baocaosucos.find((bc) => bc._id === params.id).TrangThai === true
+              ? "Hủy tiếp nhận"
+              : "Tiếp nhận"}
+          </Button>
+        </Stack>
+      ),
+    },
   ];
-  
+
   return (
     <Box sx={{ overflowX: "auto" }}>
-      <DataGrid rows={baocaosucos} columns={columns} getRowId={(row)=>row._id} getRowHeight={()=>'auto'} />
+      <DataGrid
+        rows={baocaosucos}
+        columns={columns}
+        getRowId={(row) => row._id}
+        getRowHeight={() => "auto"}
+      />
       <Dialog
         open={openDelete}
         onClose={handleCloseDeleteForm}
