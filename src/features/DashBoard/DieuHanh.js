d180661d82ed856bd8975441ea1fecb821785829
kiendt <dotrungkien6987@gmail.com>
2024-03-13 08:40:06 +0700

    13032024

--- src/features/DashBoard/DieuHanh.js
+++ src/features/DashBoard/DieuHanh.js
@@ -100,13 +100,14 @@ const DieuHanh = () => {
     // Chuyển đổi về múi giờ VN, kiểm tra đầu vào
     console.log("Chay day khong");
     if (newDate instanceof Date) {
-      newDate.setHours(7, 0, 0, 0);
+      // newDate.setHours(7, 0, 0, 0);
       setDate(new Date(newDate));
     } else if (dayjs.isDayjs(newDate)) {
       console.log("newdate", newDate);
       const updatedDate = newDate.hour(7).minute(0).second(0).millisecond(0);
       console.log("updateDate", updatedDate);
-      setDate(updatedDate);
+      setDate(newDate);
+      // setDate(updatedDate);
     }
     setIsToday(dayjs(newDate).isSame(now, 'day'));
     // dispatch(getDataNewestByNgay(date.toISOString()));
